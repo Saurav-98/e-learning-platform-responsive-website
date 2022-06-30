0d76a2999793e77f8fe08281c3d742eaa2ba625f
Saurav <sauravkumarverma22@gmail.com>
2022-07-01 02:17:32 +0530

    Popular Courses Section Completed

---
+++ app.js
@@ -0,0 +1,7 @@
+//  Change Navbar Styles on Scroll
+
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
